dfe2e65fff89cea66744b5cd0adec52bb99b55e5
rictec <riktec@gmail.com>
2015-08-29 02:40:20 +0100

    update some files

--- cm.mk
+++ cm.mk
@@ -9,13 +9,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/wender/alcatelc7/device_alcatelc7.mk)
-<<<<<<< HEAD
-=======
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_WIDTH := 480
->>>>>>> 07cd8a9bfeb6c2e911ab291768f5004db66b0ea8
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := alcatelc7
